c0a0ac2faefd73443f614b24be3e0f9ac39ad146
Renan Marangon <renan.cunha@hiplatform.com>
2019-05-31 15:27:03 -0300

    Ajustado para 000
    Usando o .to_s.rjust(3, '0')

--- create.rb
+++ create.rb
@@ -1,9 +1,10 @@
 
-n = 001
-for n in 0...100
-  f = File.new("#{n}.txt", "w")
-  for j in 1..5
-    f.puts "#{n + j}"
+
+for n in 0..100
+  filename = n.to_s.rjust(3, '0')
+  f = File.new("#{filename}.txt", "w")
+    for j in 1..5
+      f.puts (n+j).to_s.rjust(3, '0')
   end
   f.close
 end
